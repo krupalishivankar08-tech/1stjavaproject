f46b4578e4a6290635db6ce9df20949851bbbef8
HP <HP@DESKTOP-V72EKG3>
2025-12-17 12:31:53 +0530

    PCE

---
+++ src/com/javademo1/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package com.javademo1;
---
+++ src/com/studentapp/Studentappp.java
@@ -0,0 +1,6 @@
+package com.studentapp;
+public class Studentappp{
+	public static void main(String args[]) {
+		System.out.print("*");
+	}
+}
